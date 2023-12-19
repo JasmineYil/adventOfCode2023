916553c8bf3711c888ade3b2641c99019440d8d7
claristrin <71623917+ClarissaTreml@users.noreply.github.com>
2023-12-19 19:29:00 +0100

    day1 part1

--- src/day1/Main.kt
+++ src/day1/Main.kt
@@ -1,6 +1,28 @@
 package day1
 
-fun main() {
-    println("Hello World! Day 1")
+import java.io.File
 
+fun main() {
+    val puzzleFile = "src/day1/puzzleInput2.txt"
+    val puzzleInput = File(puzzleFile).readText()
+    val puzzleValues = extractPuzzleValues(puzzleInput)
+    val sum = puzzleValues.sum()
+
+    println("Puzzle values: $puzzleValues")
+    println("Sum of puzzle values: $sum")
+}
+fun extractPuzzleValues(puzzleInput: String): List<Int> {
+    val lines = puzzleInput.lines()
+    val puzzleValues = mutableListOf<Int>()
+
+    for (line in lines) {
+        val firstDigit = line.firstOrNull { it.isDigit() }
+        val lastDigit = line.lastOrNull { it.isDigit() }
+
+        if (firstDigit != null && lastDigit != null) {
+            val value = "$firstDigit$lastDigit".toInt()
+            puzzleValues.add(value)
+        }
+    }
+    return puzzleValues
 }
